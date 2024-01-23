ee9b96dcd0e0f9ba1d805bb4f2ddc6d4cd078659
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2024-01-23 01:57:05 +0000

    cahnges

--- js/custom-theme.js
+++ js/custom-theme.js
@@ -1,11 +1,13 @@
+const htmlTag = document.documentElement;
 const lightThemeIcon = document.getElementById("light-theme");
 const darkThemeIcon = document.getElementById("dark-theme");
 
 // Função para ativar o tema claro
 function activateLightTheme() {
-    document.body.classList.add("theme-transition");
+    htmlTag.setAttribute("data-theme", "light");
 
-    document.getElementById("theme-normal").disabled = false;
+    // Ativar o estilo light
+    document.getElementById("theme-light").disabled = false;
     document.getElementById("theme-dark").disabled = true;
 
     // Alternar visibilidade dos ícones
@@ -13,17 +15,24 @@ function activateLightTheme() {
     darkThemeIcon.style.display = "block";
 }
 
+// Função para ativar o tema escuro
 function activateDarkTheme() {
-    document.body.classList.add("theme-transition");
+    htmlTag.setAttribute("data-theme", "dark");
 
-    document.getElementById("theme-normal").disabled = true;
+    // Ativar o estilo dark
+    document.getElementById("theme-light").disabled = true;
     document.getElementById("theme-dark").disabled = false;
 
     darkThemeIcon.style.display = "none";
     lightThemeIcon.style.display = "block";
 }
 
-activateLightTheme();
+// Verifique o valor de data-theme na inicialização e aplique o tema apropriado
+if (htmlTag.getAttribute("data-theme") === "dark") {
+    activateDarkTheme();
+} else {
+    activateLightTheme();
+}
 
 lightThemeIcon.addEventListener("click", () => {
     activateLightTheme();
